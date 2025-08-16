63184bf8475bf6325336e9f698851a8593904a90
Goldzone93 <adam.harbeson@gmail.com>
2025-08-15 22:11:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+﻿// vite.config.js (after)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: false
-  }
+    plugins: [react()],
+    base: '/', // ✅ correct for https://goldzone93.github.io/
 })
